08a7d406f197eb4075a8d812643f6b3decdcfdf7
FachepHan <mail@fachep.com>
2020-12-13 02:27:44 +0800

    Site updated: 2020-12-13 02:27:40

--- js/gray.js
+++ js/gray.js
@@ -2,6 +2,10 @@ date=new Date();
 dateString = date.getMonth()+' '+date.getDate();
 switch(dateString) {
 	case '11 13':
-		document.documentElement.classList.add("gray");
+		document.documentElement.classList.add('gray');
 		document.styleSheets[0].addRule('*::-webkit-scrollbar-thumb','background: black');
+		var e = document.createElement('link');
+		e.rel='icon';
+		e.href='/favicon.gray.ico';
+		document.head.appendChild(e);
 }
