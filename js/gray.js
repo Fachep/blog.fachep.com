f3427843d7be0d393276d9f8edcb7aa803f8f5b5
FachepHan <mail@fachep.com>
2020-12-13 00:26:43 +0800

    Site updated: 2020-12-13 00:26:37

--- js/gray.js
+++ js/gray.js
@@ -2,5 +2,5 @@ date=new Date();
 dateString = date.getMonth()+' '+date.getDate();
 switch(dateString) {
 	case '11 13':
-		document.body.classList.add("gray");
+		document.documentElement.classList.add("gray");
 }
