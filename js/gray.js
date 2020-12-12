7e89f58212fd7583ba147639c9f6dc4c012534d8
FachepHan <mail@fachep.com>
2020-12-13 00:48:29 +0800

    Site updated: 2020-12-13 00:48:25

--- js/gray.js
+++ js/gray.js
@@ -3,4 +3,5 @@ dateString = date.getMonth()+' '+date.getDate();
 switch(dateString) {
 	case '11 13':
 		document.documentElement.classList.add("gray");
+		document.styleSheets[0].addRule('*::-webkit-scrollbar-thumb','background: black');
 }
